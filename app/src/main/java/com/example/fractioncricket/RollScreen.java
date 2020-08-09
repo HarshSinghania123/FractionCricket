c2989651619a401e4e640ad14cbab78457bf34ed
HarshSinghania123 <harshsinghania080697@gmail.com>
2020-08-09 21:06:12 +0530

    BugFixes commit

--- app/src/main/java/com/example/fractioncricket/RollScreen.java
+++ app/src/main/java/com/example/fractioncricket/RollScreen.java
@@ -1,42 +1,40 @@
 package com.example.fractioncricket;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
-import pl.droidsonroids.gif.GifImageView;
-
 public class RollScreen extends AppCompatActivity {
 
-//    GifImageView throwImg;
     ImageButton btnBall, btnBat;
     ImageView questionImage;
-//    Animation mBounceAnimation;
     Intent intent;
     EditText dividendTxt, divisorTxt;
     int scoresCount = 0 , wicketsCount = 10;
-    String result, dividend, divisor, imgName  ;
+    String result, dividend, divisor, imgNameStr  ;
     HashMap<String, List<String>> fractionMap;
     HashMap<String, List<String>> fractionMapCopy;
-    int imageName[] = new int [7];
+    Integer imageName[] = new Integer [6];
+    List<Integer> randomizeArrayList;
+    Integer randomTempNum [] = new Integer [6];
     Random rand;
     TextView scoreTxt, wicketTxt;
+    Object tempObject ;
+
+    int iterator = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +44,7 @@ public class RollScreen extends AppCompatActivity {
         btnBat = findViewById(R.id.imageButton_bat);
         scoreTxt = findViewById(R.id.score);
         wicketTxt = findViewById(R.id.wicket);
+        iterator = 0;
 
         rand = new Random();
         fractionMap = new HashMap<>();
@@ -53,6 +52,14 @@ public class RollScreen extends AppCompatActivity {
         dividendTxt = findViewById(R.id.dividendText);
         divisorTxt = findViewById(R.id.divisorText);
         questionImage = findViewById(R.id.question_image);
+
+
+//        ********************Random Set Generation****************
+        for (int i = 0; i<6; i++){
+            randomTempNum[i] = i+1;
+        }
+
+//        *************************Saving the results of the image in a hashmap*************
         fractionMap.put("img_1", Arrays.asList(new String[] {"1/5", "1/6", "2/5"}));
         fractionMap.put("img_2", Arrays.asList(new String[] {"2/5", "2/6", "1/3"}));
         fractionMap.put("img_3", Arrays.asList(new String[] {"4/5", "5/6", "3/5"}));
@@ -61,6 +68,7 @@ public class RollScreen extends AppCompatActivity {
         fractionMap.put("img_6", Arrays.asList(new String[] {"5/4"}));
 //        fractionMap.put("img_7", Arrays.asList(new String[] {"6/5"}));
 
+//        *********************Image location storing array****************
         imageName[0]= R.drawable.img_1;
         imageName[1]= R.drawable.img_2;
         imageName[2]= R.drawable.img_3;
@@ -68,52 +76,61 @@ public class RollScreen extends AppCompatActivity {
         imageName[4]= R.drawable.img_5;
         imageName[5]= R.drawable.img_6;
 
-//        Creating a copy of the original map for processing
 
-        fractionMapCopy = fractionMap;
+//        ***************************Randamization of list*************************
+        randomizeArrayList = Arrays.asList(randomTempNum);
+        Collections.shuffle(randomizeArrayList);
+        randomTempNum = randomizeArrayList.toArray(randomTempNum);
 
-//
+
+//      ******************* click event of BALL button*********************
 
         btnBall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imgName = "img_";
-//                dividendTxt.setEnabled(true);
-//                divisorTxt.setEnabled(true);
-                //btnBall.setVisibility(View.INVISIBLE);
-//                btnBat.setVisibility(View.VISIBLE);
+                imgNameStr = "img_";
 
-//                Image Selection
-                int randomImage = (rand.nextInt(6)); //modulo with the no of images given to get the number within the range and start wwith the index 1
-                questionImage.setImageResource(imageName[randomImage]); // obtaining the particular image for the based on the above number
-                imgName = imgName.concat(""+(randomImage+1));
-                Toast.makeText(getApplicationContext(), imgName, Toast.LENGTH_LONG).show();
+//                *************************Image Selection  ***************************
+                questionImage.setImageResource(imageName[randomTempNum[iterator]-1]);
+                imgNameStr = imgNameStr.concat(""+(randomTempNum[iterator]));
+                btnBat.setVisibility(View.VISIBLE);
+                btnBall.setVisibility(View.INVISIBLE);
             }
         });
 
+//        *********************Click event of BAT button ********************
         btnBat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dividend = (dividendTxt.getText().toString());
                 divisor = (divisorTxt.getText().toString());
                 result = ""+dividend + "/" +divisor;
-                divisorTxt.setText("");
-                dividendTxt.setText("");
-
-                intent = new Intent(getApplicationContext(), QuestionScreen.class);
-                if(fractionMap.get(imgName).contains(result)) {
-                    intent.putExtra("result", "win");
-                    scoresCount += 6;
-                    scoreTxt.setText(""+scoresCount);
 
 
+//                *********************Validation for Empty edits click event***********************
+                if (!((dividend.isEmpty() && divisor.isEmpty() ) || (dividend.equals(" ") && divisor.equals(" ")))) {
+                    intent = new Intent(getApplicationContext(), QuestionScreen.class);
+                    if(fractionMap.get(imgNameStr).contains(result)) {
+                        intent.putExtra("result", "win");
+                        scoresCount += 6;
+                        scoreTxt.setText(""+scoresCount);
+                    }
+                    else{
+                        intent.putExtra("result","out");
+                        wicketsCount -= 1;
+                        wicketTxt.setText(""+wicketsCount);
+                    }
+                    startActivity(intent);
+                    questionImage.setImageResource(R.drawable.ball_throwing);
+                    imgNameStr = "";
+                    result = "";
+                    divisorTxt.setText("");
+                    dividendTxt.setText("");
                 }
-                else{
-                    intent.putExtra("result","out");
-                    wicketsCount -= 1;
-                    wicketTxt.setText(""+wicketsCount);
-                }
-                startActivity(intent);
+                btnBat.setVisibility(View.INVISIBLE);
+                btnBall.setVisibility(View.VISIBLE);
+                if(iterator < 6){
+                iterator++;}
             }
         });
 //         *****************GAME OVER****************
